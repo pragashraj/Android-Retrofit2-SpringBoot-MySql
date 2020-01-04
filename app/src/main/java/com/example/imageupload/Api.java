7b5b072ef480773448e5a483d33b88edd80fd6b6
S.Pragashraj <rajpragashraj30@gmail.com>
2020-01-04 11:08:31 +0530

    first commit

---
+++ app/src/main/java/com/example/imageupload/Api.java
@@ -0,0 +1,13 @@
+package com.example.imageupload;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface Api {
+    @Multipart
+    @POST("ocr")
+    Call<MyResponse> uploadAttachment(@Part MultipartBody.Part filePart);
+}
---
+++ app/src/main/java/com/example/imageupload/MyResponse.java
@@ -0,0 +1,13 @@
+package com.example.imageupload;
+
+class MyResponse {
+    private String success;
+
+    public MyResponse(String success) {
+        this.success = success;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+}
